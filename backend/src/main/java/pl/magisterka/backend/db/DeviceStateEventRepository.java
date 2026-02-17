ad3a0e3554ea5d5fdb7f8e6f31e9e56f29599817
igorg <igorgula17@gmail.com>
2026-02-17 15:57:11 +0100

    Apka przed tworzeniem REST

--- backend/src/main/java/pl/magisterka/backend/db/DeviceStateEventRepository.java
+++ backend/src/main/java/pl/magisterka/backend/db/DeviceStateEventRepository.java
@@ -1,5 +1,19 @@
 package pl.magisterka.backend.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface DeviceStateEventRepository extends JpaRepository<DeviceStateEventEntity, Long> {}
+import java.util.List;
+import java.util.Optional;
+
+public interface DeviceStateEventRepository extends JpaRepository<DeviceStateEventEntity, Long> {
+
+    Optional<DeviceStateEventEntity> findTopByDeviceIdOrderByTsDesc(String deviceId);
+
+    @Query(value = """
+        select distinct on (device_id) *
+        from device_state_event
+        order by device_id, ts desc
+    """, nativeQuery = true)
+    List<DeviceStateEventEntity> findLatestStatePerDevice();
+}
--- backend/src/main/java/pl/magisterka/backend/db/EnergyTelemetryRepository.java
+++ backend/src/main/java/pl/magisterka/backend/db/EnergyTelemetryRepository.java
@@ -1,5 +1,21 @@
 package pl.magisterka.backend.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface EnergyTelemetryRepository extends JpaRepository<EnergyTelemetryEntity, Long> {}
+import java.util.List;
+import java.util.Optional;
+
+public interface EnergyTelemetryRepository extends JpaRepository<EnergyTelemetryEntity, Long> {
+
+    // Najnowszy pomiar dla konkretnego deviceId
+    Optional<EnergyTelemetryEntity> findTopByDeviceIdOrderByTsDesc(String deviceId);
+
+    // Najnowszy pomiar dla każdego deviceId (1 rekord na device)
+    @Query(value = """
+        select distinct on (device_id) *
+        from energy_telemetry
+        order by device_id, ts desc
+    """, nativeQuery = true)
+    List<EnergyTelemetryEntity> findLatestTelemetryPerDevice();
+}
